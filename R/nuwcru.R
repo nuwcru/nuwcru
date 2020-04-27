6bab2d8b834cc464df82a8b1af8c48a5a67ba689
emhedlin <hedlin@ualberta.ca>
2020-04-27 12:22:32 -0600

    added file copying function and bw logo

--- R/nuwcru.R
+++ R/nuwcru.R
@@ -1,4 +1,13 @@
 
+list.of.packages <- c("dplyr", "stringr", "lubridate")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+library(dplyr)
+library(stringr)
+library(lubridate)
+
+
+
 # plot themes  --------------------------------------------
 
 theme_nuwcru <- function(){
@@ -189,6 +198,48 @@ cam_acc_lik <- function(a, b, L){
 
 }
 
+
+# File Manipulation -------------------------------------------------------
+
+# copy Audio files from specific times of the day over to new directory
+
+file_copy <- function(source = "/Volumes/LACIE/QAM/ARU Recordings/2015/",    # source directory
+                      dest   = "/Volumes/LACIE/Wildtrax upload 1",
+                      site = "R1Z1",
+                      start = 1,         # start time
+                      end = 11,           # end time
+                      create_dir = FALSE
+){
+
+  files <- list.files(paste(source, site, sep = ""), ".wav$")
+
+  date <- str_sub(files, 6, -5)
+  date <- str_replace_all(date, "_", " ")
+  str_sub(date,12,2) <- ":"
+  str_sub(date,15,2) <- ":"
+  str_sub(date,5,0) <- "/"
+  str_sub(date,8,0) <- "/"
+  date <- ymd_hms(date)
+  hour <- hour(date)
+  files <- data.frame(filename = files,
+                     date = date,
+                     hour = hour)
+
+  str_sub(files$filename, 0, 0) <- paste(source,"/",site,"/", sep = "")
+
+  files_up <- files %>% filter(hour > start & hour < end)
+
+  if (create_dir == TRUE) {
+    dir.create(paste(dest, "/",site, sep = ""))
+    file.copy(files_up$filename,
+              paste(dest, "/",site, sep = ""))} else {
+    file.copy(files$filename,
+            paste(dest, "/",site, sep = ""))
+  }
+
+  message(paste(length(files_up$filename), "files moved to new directory"))
+
+}
 
 
 
